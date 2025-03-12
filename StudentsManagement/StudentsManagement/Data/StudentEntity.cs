acd28367bd32cf9f1ac42e4a05a9e03dc165d90b
Valery Raikov <124359973+ValeryRaikov@users.noreply.github.com>
2025-03-12 09:51:38 +0200

    Create Student model

---
+++ StudentsManagement/StudentsManagement/Data/StudentEntity.cs
@@ -0,0 +1,23 @@
+﻿using SQLite;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentsManagement.Data
+{
+    public class StudentEntity
+    {
+        [PrimaryKey, AutoIncrement]
+        public int StudentId { get; set; }
+
+        [Required]
+        [System.ComponentModel.DataAnnotations.MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [System.ComponentModel.DataAnnotations.MaxLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
---
+++ StudentsManagement/StudentsManagement/Models/Student.cs
@@ -0,0 +1,34 @@
+﻿namespace StudentsManagement.Models
+{
+    public class Student
+    {
+        private int _studentId;
+        private string _firstName;
+        private string _lastName;
+        private string _email;
+
+        public int StudentId
+        {
+            get { return _studentId; }
+            set { _studentId = value; }
+        }
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+    }
+}
