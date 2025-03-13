3fdfcbac271faa119432201c43e89da15cdf432c
Valery Raikov <124359973+ValeryRaikov@users.noreply.github.com>
2025-03-13 10:35:33 +0200

    Implement entity inheritance

--- StudentsManagement/StudentsManagement/Data/StudentEntity.cs
+++ StudentsManagement/StudentsManagement/Data/StudentEntity.cs
@@ -1,24 +1,25 @@
-﻿using SQLite;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
+using SQLite;
+using StudentsManagement.Models;
 
 namespace StudentsManagement.Data
 {
     [Table("students")]
-    public class StudentEntity
+    public class StudentEntity : Student
     {
         [PrimaryKey, AutoIncrement]
         public int StudentId { get; set; }
 
         [Required]
         [System.ComponentModel.DataAnnotations.MaxLength(50)]
-        public string FirstName { get; set; }
+        public new string FirstName { get; set; }
 
         [Required]
         [System.ComponentModel.DataAnnotations.MaxLength(50)]
-        public string LastName { get; set; }
+        public new string LastName { get; set; }
 
         [Required]
         [EmailAddress]
-        public string Email { get; set; }
+        public new string Email { get; set; }
     }
 }
